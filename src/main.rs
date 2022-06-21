6d6441b79b327ac56200b2f11fc77cf46e78ee11
Rodion Belovitskiy <belovickiy.rodion@gmail.com>
2022-06-21 19:39:25 +0000

    feat: add dependencies for networking

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 #![feature(test)]
 
-mod katas;
-
 extern crate test;
 
-fn main() {
-    println!("Codewars training project");
+use std::collections::HashMap;
+
+mod katas;
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+
+    println!("{:#?}", resp);
+    
+    Ok(())
 }
